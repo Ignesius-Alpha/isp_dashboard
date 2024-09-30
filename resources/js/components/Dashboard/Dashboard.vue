757c9b8e00d12d32a489f57175127f023cd4d4d0
IKMorena <Kwenaimorena@gmail.com>
2024-09-30 09:26:02 +0200

    "Added new CSS class 'text-9xl' and updated font sizes in Dashboard.vue component"

--- resources/js/components/Dashboard/Dashboard.vue
+++ resources/js/components/Dashboard/Dashboard.vue
@@ -4,7 +4,7 @@
             <div
                 class="flex border-2 border-white rounded-[50px] text-center py-16 p-5 bg-white bg-opacity-20 justify-center">
                 <div class="text-center">
-                    <h3 class="font-quicksand font-bold text-8xl text-[#3380F5]">156</h3>
+                    <h3 class="font-quicksand font-bold text-9xl text-[#3380F5]">156</h3>
                     <p class="text-[#173D6B] text-lg mb-3 lg:mb-8">Active Services</p>
                     <a href="/Activeservices"
                         class="border-2 border-[#173D6B] rounded-[30px] px-8 font-medium py-1.5 text-[#173D6B]">View</a>
@@ -13,13 +13,13 @@
             <div class="grid grid-cols-2 gap-x-4 xl:gap-x-8">
                 <a href="" class="flex col-span-1 justify-center items-center rounded-[50px] py-16 p-5 bg-[#3380F5]">
                     <div class="text-center">
-                        <h3 class="font-quicksand font-bold text-8xl text-white">153</h3>
+                        <h3 class="font-quicksand font-bold text-9xl text-white">153</h3>
                         <p class="text-white text-lg">Online Services</p>
                     </div>
                 </a>
                 <a href="" class="flex col-span-1 justify-center items-center rounded-[50px] py-16 p-5 bg-[#173D6B]">
                     <div class="text-center">
-                        <h3 class="font-quicksand font-bold text-8xl text-white">3</h3>
+                        <h3 class="font-quicksand font-bold text-9xl text-white">3</h3>
                         <p class="text-white text-lg">Offline Services</p>
                     </div>
                 </a>
@@ -42,7 +42,7 @@
             </div>
             <div class="rounded-[50px] flex col-span-1 text-center py-12 bg-[#3380F5] justify-center">
                 <div class="flex flex-col justify-center items-center place-items-center text-center">
-                    <h3 class="font-quicksand font-bold text-8xl text-white">12</h3>
+                    <h3 class="font-quicksand font-bold text-9xl text-white">12</h3>
                     <p class="text-white text-lg mb-3 lg:mb-8">Pending Orders</p>
                     <a href="/Activeservices"
                         class="border-2 border-white rounded-[30px] px-8 font-medium py-1.5 text-white">View</a>
