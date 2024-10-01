54ade344cc72abad62cf9f63bd5896e930c5b0f0
IKMorena <Kwenaimorena@gmail.com>
2024-10-01 08:57:39 +0200

    "Updated form class in Linksearch.vue to change margin between elements"

--- resources/js/components/Linksearch/Linksearch.vue
+++ resources/js/components/Linksearch/Linksearch.vue
@@ -3,7 +3,7 @@
     <h2 class="font-bold text-2xl">Quick Link Search</h2>
     <p class="text-md font-medium pb-7">This Interface allows you to search for Links based on the criteria
         specified below.</p>
-    <form class="flex w-full lg:flex-row md:justify-between md:space-x-1 space-y-5 md:space-y-0">
+    <form class="flex w-full lg:flex-row md:justify-between md:space-x-3 space-y-5 md:space-y-0">
         <div class="flex flex-col w-[24%]">
             <label class="font-medium lg:font-bold ml-6 truncate" for="custName">Customer Full Name</label>
             <input v-model="custName" id="custName" :disabled="ispOrderNum !== ''" :class="ispOrderNum !== '' ? 'border-[#ff3a3a] border-opacity-50 placeholder:text-[#ff3a3a]' : 'border-[#173D6B] border-opacity-10'" class="rounded-[30px] mt-2 py-2 bg-transparent border pl-6" :placeholder="ispOrderNum !== '' ? 'Disabled' : 'Peter Parker'" type="text">
